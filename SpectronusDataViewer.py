56fdebdb6ed9b977f48b717b72827f9961126d3a
grahamk <philotrum@gmail.com>
2015-10-06 16:12:39 +1100

    Added N2 cylinder pressures to the system state plot

--- SpectronusDataViewer.py
+++ SpectronusDataViewer.py
@@ -71,90 +71,57 @@ class SpectronusData_Dialog(Frame):
         startDate = self.frame.startDateTXT.get()
         finishDate = self.frame.finishDateTXT.get()
 
-        if (NumTables(databaseFilename) < 3):
-            # It is the old database with all the data in one table.
-            selectSTR = 'SELECT Collection_Start_Time,CO2, CO2_1,CO2_2,CV_del13C,CO,CH4,N2O,H2O,'
-            selectSTR += 'Cell_Temperature_Avg,Room_Temperature_Avg,Cell_Pressure_Avg,Flow_In_Avg,Flow_Out_Avg '
-            selectSTR += 'FROM sysvariables where Collection_Start_Time between '
-            selectSTR += chr(39) + startDate + chr(39) + ' and ' + chr(39) +  finishDate + chr(39)
-            if (databaseFilename.find('Eddy') != -1):
-                selectSTR += ' and Collection_Start_Time Not between ' + chr(39) + '2014-07-04 04:45' + chr(39)
-                selectSTR += ' and ' + chr(39) + '2014-07-04 13:15' + chr(39)
-            #selectSTR += ' and Flow_In_Avg > 0.4'
-            #selectSTR += ' and Inlet = ' + chr(39) + 'Inlet_3' + chr(39)
-            #if (databaseFilename.find('Darwin') != -1):
-            #    selectSTR += ' and Cell_Pressure_Avg > 1098'
-            print selectSTR
+        # Primary key and date/time from sysvariables
+        selectSTR = 'SELECT  sysvariablesPK, Collection_Start_Time FROM sysvariables where Collection_Start_Time between '
+        selectSTR += chr(39) + startDate + chr(39) + ' and ' + chr(39) +  finishDate + chr(39)
+        rows1 = ReadDatabase(databaseFilename, selectSTR)
+        sysvariablesPK = LoadData(rows1, 0)
 
-            rows = ReadDatabase(databaseFilename, selectSTR)
+        readStartPos = str(sysvariablesPK[0])
+        readFinishPos = str(sysvariablesPK[len(sysvariablesPK) -1])
 
-            Date = ConvertToDateTime(rows, 0)
-            CO2 = LoadData(rows, 1)
-            CO2_12 = LoadData(rows, 2)
-            CO2_13 = LoadData(rows, 3)
-            Del13C = LoadData(rows, 4)
-            CO = LoadData(rows, 5)
-            CH4 = LoadData(rows, 6)
-            N2O = LoadData(rows, 7)
-            H2O = LoadData(rows, 8)
-            CellTemp = LoadData(rows, 9)
-            RoomTemp = LoadData(rows, 10)
-            CellPress = LoadData(rows, 11)
-            FlowIn = LoadData(rows, 12)
-            FlowOut = LoadData(rows, 13)
+        # Uncorrected species.
+        selectSTR = 'SELECT CO2, CO2_1, CO2_2, CH4, N2O, CO, H2O FROM analysisprimary '
+        selectSTR += 'where analysisprimaryID between ' + readStartPos + ' and ' + readFinishPos
+        rows2 = ReadDatabase(databaseFilename, selectSTR)
 
-        else:
-            # It is the new database with multiple tables.
+        # Calculated vals
+        selectSTR = 'SELECT CV_del13C FROM calcvals where calcvalsID between ' + readStartPos + ' and ' + readFinishPos
+        rows3 = ReadDatabase(databaseFilename, selectSTR)
 
-            # Primary key and date/time from sysvariables
-            selectSTR = 'SELECT  sysvariablesPK, Collection_Start_Time FROM sysvariables where Collection_Start_Time between '
-            selectSTR += chr(39) + startDate + chr(39) + ' and ' + chr(39) +  finishDate + chr(39)
-            rows1 = ReadDatabase(databaseFilename, selectSTR)
-            sysvariablesPK = LoadData(rows1, 0)
+        # AI averages
+        selectSTR = 'SELECT Cell_Temperature_Avg,Room_Temperature_Avg,Cell_Pressure_Avg,Flow_In_Avg,Flow_Out_Avg,  '
+        selectSTR += 'Tank_Hi_Avg, Tank_Lo_Avg FROM aiaverages where aiaveragesID between '
+        selectSTR += readStartPos + ' and ' + readFinishPos
+        rows4 = ReadDatabase(databaseFilename, selectSTR)
 
-            readStartPos = str(sysvariablesPK[0])
-            readFinishPos = str(sysvariablesPK[len(sysvariablesPK) -1])
+        # Assemble all data into a single list
+        fullData = []
+        for i in range(0, len(rows1) -1):
+            fullData.append(rows1[i] + rows2[i] + rows3[i] + rows4[i])
 
-            # Uncorrected species.
-            selectSTR = 'SELECT CO2, CO2_1, CO2_2, CH4, N2O, CO, H2O FROM analysisprimary '
-            selectSTR += 'where analysisprimaryID between ' + readStartPos + ' and ' + readFinishPos
-            rows2 = ReadDatabase(databaseFilename, selectSTR)
+        # Filter out lines that don't have all the data
+        filteredData = []
+        for row in fullData:
+            if (str(row).find('None') == -1):
+                filteredData.append(row)
 
-            # Calculated vals
-            selectSTR = 'SELECT CV_del13C FROM calcvals where calcvalsID between ' + readStartPos + ' and ' + readFinishPos
-            rows3 = ReadDatabase(databaseFilename, selectSTR)
-
-            # AI averages
-            selectSTR = 'SELECT Cell_Temperature_Avg,Room_Temperature_Avg,Cell_Pressure_Avg,Flow_In_Avg,Flow_Out_Avg '
-            selectSTR += 'FROM aiaverages where aiaveragesID between '
-            selectSTR += readStartPos + ' and ' + readFinishPos
-            rows4 = ReadDatabase(databaseFilename, selectSTR)
-
-            # Assemble all data into a single list
-            fullData = []
-            for i in range(0, len(rows1) -1):
-                fullData.append(rows1[i] + rows2[i] + rows3[i] + rows4[i])
-
-            # Filter out lines that don't have all the data
-            filteredData = []
-            for row in fullData:
-                if (str(row).find('None') == -1):
-                    filteredData.append(row)
-
-            Date = ConvertToDateTime(filteredData, 1)
-            CO2 = LoadData(filteredData, 2)
-            CO2_12 = LoadData(filteredData, 3)
-            CO2_13 = LoadData(filteredData, 4)
-            CH4 = LoadData(filteredData, 5)
-            N2O = LoadData(filteredData, 6)
-            CO = LoadData(filteredData, 7)
-            H2O = LoadData(filteredData, 8)
-            Del13C = LoadData(filteredData, 9)
-            CellTemp = LoadData(filteredData, 10)
-            RoomTemp = LoadData(filteredData, 11)
-            CellPress = LoadData(filteredData, 12)
-            FlowIn = LoadData(filteredData, 13)
-            FlowOut = LoadData(filteredData, 14)
+        Date = ConvertToDateTime(filteredData, 1)
+        CO2 = LoadData(filteredData, 2)
+        CO2_12 = LoadData(filteredData, 3)
+        CO2_13 = LoadData(filteredData, 4)
+        CH4 = LoadData(filteredData, 5)
+        N2O = LoadData(filteredData, 6)
+        CO = LoadData(filteredData, 7)
+        H2O = LoadData(filteredData, 8)
+        Del13C = LoadData(filteredData, 9)
+        CellTemp = LoadData(filteredData, 10)
+        RoomTemp = LoadData(filteredData, 11)
+        CellPress = LoadData(filteredData, 12)
+        FlowIn = LoadData(filteredData, 13)
+        FlowOut = LoadData(filteredData, 14)
+        Tank_Hi = LoadData(filteredData, 15)
+        Tank_Lo = LoadData(filteredData, 16)
 
         ConcentrationsFig = plt.figure('Concentration retrievals')
         ConcentrationsFig.subplots_adjust(hspace=0.1)
@@ -227,14 +194,14 @@ class SpectronusData_Dialog(Frame):
         SystemStateFig.suptitle(databaseFilename + '\n' + str(Date[0]) + ' to ' + str(Date[len(Date) -1]), fontsize=14, fontweight='bold')
 
          # Cell Pressure
-        Ax1=SystemStateFig.add_subplot(411)
+        Ax1=SystemStateFig.add_subplot(611)
         Ax1.scatter(Date,CellPress, marker='+')
         Ax1.set_ylabel('Cell Pressure')
         Ax1.grid(True)
         Ax1.get_yaxis().get_major_formatter().set_useOffset(False)
 
         # Cell Temperature
-        Ax2=SystemStateFig.add_subplot(412, sharex=Ax1)
+        Ax2=SystemStateFig.add_subplot(612, sharex=Ax1)
         Ax2.scatter(Date,CellTemp, marker='+', label='Cell Temp')
         Ax2.set_ylabel('Cell Temperature')
         Ax2.yaxis.set_label_position("right")
@@ -243,14 +210,14 @@ class SpectronusData_Dialog(Frame):
         Ax2.get_yaxis().get_major_formatter().set_useOffset(False)
 
         # Room Temperature
-        Ax3=SystemStateFig.add_subplot(413, sharex=Ax1)
+        Ax3=SystemStateFig.add_subplot(613, sharex=Ax1)
         Ax3.scatter(Date,RoomTemp, marker='+', label='Room Temp')
         Ax3.set_ylabel('Room Temperature')
         Ax3.grid(True)
         Ax3.get_yaxis().get_major_formatter().set_useOffset(False)
 
         # Cell flow
-        Ax4=SystemStateFig.add_subplot(414, sharex=Ax1)
+        Ax4=SystemStateFig.add_subplot(614, sharex=Ax1)
         Ax4.scatter(Date,FlowIn, marker='+', label='Flow In',color='r')
         Ax4.scatter(Date,FlowOut, marker='+', label='Flow Out',color='b')
         Ax4.set_ylabel('Cell Flows')
@@ -261,11 +228,28 @@ class SpectronusData_Dialog(Frame):
         leg.get_frame().set_alpha(0.5)
         Ax4.get_yaxis().get_major_formatter().set_useOffset(False)
 
+        # Cylinder high pressures
+        Ax5=SystemStateFig.add_subplot(615, sharex=Ax1)
+        Ax5.scatter(Date,Tank_Hi, marker='+', label='Tank_Hi')
+        Ax5.set_ylabel('Cylinder Pressure High')
+        Ax5.grid(True)
+        Ax5.get_yaxis().get_major_formatter().set_useOffset(False)
+
+        Ax6=SystemStateFig.add_subplot(616, sharex=Ax1)
+        Ax6.scatter(Date,Tank_Lo, marker='+', label='Tank_Lo')
+        Ax6.set_ylabel('Cylinder Pressure Low')
+        Ax6.yaxis.set_label_position("right")
+        Ax6.yaxis.tick_right()
+        Ax6.grid(True)
+        leg = plt.legend(loc=2,ncol=1, fancybox = True)
+        leg.get_frame().set_alpha(0.5)
+        Ax6.get_yaxis().get_major_formatter().set_useOffset(False)
+
         # Set x axis range
         t0 = Date[0] - dt.timedelta(0,3600)
         t1= Date[len(Date) -1 ] + dt.timedelta(0,3600)
-        Ax4.set_xlim(t0,t1)
-        Ax4.grid(True)
+        Ax6.set_xlim(t0,t1)
+        Ax6.grid(True)
 
         SystemStateFig.autofmt_xdate()
         plt.show()
